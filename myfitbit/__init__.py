133864c5c88950d85ee411bd3cf5c1e6dd5c5507
Tom Flanagan <tom@zoox.com>
2017-12-24 17:36:17 -0800

    Use OAuth2

--- myfitbit/__init__.py
+++ myfitbit/__init__.py
@@ -1,104 +1,122 @@
 from ._version import __version__, version
 
+import base64
 import re
+import os
 import json
+import webbrowser
+import urllib.parse
 
 import requests
 from bs4 import BeautifulSoup
+import http.server
 
-__all__ = ['Fitbit']
+__all__ = ['Fitbit', 'FitbitAuth']
 
-LOGIN_URL = 'https://www.fitbit.com/login'
+
+class RedirectServer(object):
+    IP = '127.0.0.1'
+    PORT = 8189
+    URL = 'http://localhost:{}/'.format(PORT)
+    def __init__(self):
+        self.result = None
+        class HTTPHandler(http.server.BaseHTTPRequestHandler):
+          def do_GET(s):
+                url = urllib.parse.urlparse(s.path)
+                query = urllib.parse.parse_qs(url.query)
+                self.result = query
+                s.send_response(200)
+                s.send_header('Content-type', 'text/html')
+                s.end_headers()
+                s.wfile.write(b'OK<script>window.close();</script>')
+        self.handler_class = HTTPHandler
+
+    def get_result(self):
+        httpd = http.server.HTTPServer(('127.0.0.1', self.PORT), self.handler_class)
+        httpd.timeout = 30
+        httpd.handle_request()
+        if not self.result:
+            raise RuntimeError('Failed to auth with browser')
+        return self.result
+
+
+class FitbitAuth(object):
+    ACCESS_TOKEN_FILE = '.myfitbit_access_token'
+    def __init__(self, client_id, client_secret):
+        self.client_id = client_id
+        self.client_secret = client_secret
+        self.access_token = None
+
+    def get_auth_code(self):
+        url = 'https://www.fitbit.com/oauth2/authorize?' + \
+            '&'.join('{}={}'.format(k, v) for k, v in {
+                'response_type': 'code',
+                'client_id': self.client_id,
+                'redirect_uri': RedirectServer.URL,
+                'scope': '%20'.join(('activity', 'heartrate', 'location', 'nutrition', 'profile', 'settings', 'sleep', 'social', 'weight')),
+                'expires_in': '31536000',
+            }.items())
+
+        webbrowser.open_new(url)
+        r = RedirectServer().get_result()
+        return r['code'][0]
+
+    def get_access_token(self):
+        auth_code = self.get_auth_code()
+        auth_string = base64.b64encode(
+            self.client_id.encode('ascii') + b':' + self.client_secret.encode('ascii')).decode('ascii')
+        r = requests.post('https://api.fitbit.com/oauth2/token',
+                headers={
+                    'Authorization': 'Basic ' + auth_string,
+                },
+                data={
+                    'clientId': self.client_id,
+                    'code': auth_code,
+                    'grant_type': 'authorization_code',
+                    'redirect_uri': RedirectServer.URL,
+                },
+                timeout=30
+        )
+        r.raise_for_status()
+        return json.loads(r.text)
+
+    def ensure_access_token(self):
+        if self.access_token:
+            return
+        if os.path.isfile(self.ACCESS_TOKEN_FILE):
+            self.access_token = json.load(open(self.ACCESS_TOKEN_FILE))
+            return
+        self.get_auth_code()
+        self.access_token = self.get_access_token()
+        with open(self.ACCESS_TOKEN_FILE, 'w') as f:
+            json.dump(self.access_token, f, sort_keys=True, indent=2)
+
 
 class Fitbit(object):
-    def __init__(self):
+    def __init__(self, access_token):
+        self.access_token = access_token
         self.session = requests.Session()
-        self.user_info = None
+        self.session.timeout = 30
+        self.session.headers['Authorization'] = 'Bearer ' + access_token
+        self.profile = self.get_profile()
 
     @property
     def user_id(self):
-        return self.user_info['encodedId']
+        return self.profile['encodedId']
 
-    @property
-    def api_url(self):
-        return 'https://{}'.format(self.user_info['apiUrl'])
-
-    @property
-    def authorization(self):
-        return self.user_info['oauth2Token']
-
-    def login(self, username, password):
-        req = self.session.get(LOGIN_URL, headers={'Referer': LOGIN_URL})
-        login_page = BeautifulSoup(req.text, 'html.parser')
-
-        source_page = \
-            login_page.find('input', {'name':'_sourcePage'}).get('value')
-
-        fp = \
-            login_page.find('input', {'name':'__fp'}).get('value')
-
-        login_data = {
-            'login': 'Log+In',
-            'includeWorkflow': '',
-            'redirect': '',
-            'switchToNonSecureOnRedirect': '',
-            'disableThirdPartyLogin': 'false',
-            'email': username,
-            'password': password,
-            'rememberMe': 'true',
-            '_sourcePage': source_page,
-            '__fp': fp,
-        }
-
-        req = self.session.post(LOGIN_URL, data=login_data, headers={'Referer': LOGIN_URL})
-
-        dashboard = BeautifulSoup(req.text, 'html.parser')
-
-        user_script = \
-            dashboard.find('div', {'id':'dash'}).next_element.next_element.text
-
-        user_json = re.match(r'^[^{]*(\{.*\})[^}]*$', user_script).group(1)
-        self.user_info = json.loads(user_json)
-
+    def get_profile(self):
+        r = self.session.get('https://api.fitbit.com/1/user/-/profile.json')
+        r.raise_for_status()
+        return json.loads(r.text)['user']
 
     def get_sleep(self, date):
-        req = self.session.get(
-            '{api_url}/1.2/user/{user_id}/sleep/date/{date}.json?date={date}&id={user_id}'.format(
-                api_url=self.api_url,
-                user_id=self.user_id,
-                date=str(date),
-            ),
-            headers={
-                'Referer': 'https://www.fitbit.com/sleep',
-                'Authorization': 'Bearer ' + self.authorization}
-        )
-
-        return req.json()
+        r = self.session.get('https://api.fitbit.com/1.2/user/{}/sleep/date/{}.json'
+            .format(self.user_id, str(date)))
+        r.raise_for_status()
+        return json.loads(r.text)['sleep']
 
     def get_heartrate(self, date):
         raise NotImplementedError
 
-        # https://www.fitbit.com/ajaxapi
-        #
-        # POST /ajaxapi HTTP/1.1
-        # Host: www.fitbit.com
-        # User-Agent: Mozilla/5.0 (Windows NT 10.0; WOW64; rv:52.0) Gecko/20100101 Firefox/52.0
-        # Accept: */*
-        # Accept-Language: en-US,en;q=0.5
-        # Accept-Encoding: gzip, deflate, br
-        # Content-Type: application/x-www-form-urlencoded; charset=UTF-8
-        # X-Requested-With: XMLHttpRequest
-        # Referer: https://www.fitbit.com/2017/04/09
-
-        # request={"template":"/ajaxTemplate.jsp","serviceCalls":[{"name":"activityTileData","args":{"date":"2017-04-08","dataTypes":"heart-rate"},"method":"getIntradayData"}]}
-        # csrfToken=UUID
-
-
-
     def get_steps(self):
         raise NotImplementedError
-
-    # https://web-api.fitbit.com/1/user/5M5DHS/activities/heart/date/2017-04-09/2017-04-02.json
-    # https://web-api.fitbit.com/1/user/5M5DHS/activities/date/2017-04-09.json
-    # https://web-api.fitbit.com/1.1/user/5M5DHS/sed/date/2017-04-09.json
-    # https://web-api.fitbit.com/1.2/user/-/sleep/date/2017-04-09.json
--- myfitbit/__main__.py
+++ myfitbit/__main__.py
@@ -1,26 +1,36 @@
 from datetime import date
 import argparse
+import configparser
 import getpass
+import logging
 import json
 
-from . import Fitbit
+logging.basicConfig(level=logging.DEBUG)
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--username', required=True)
-    parser.add_argument('--ask-password', required=True, action='store_true')
-    args = parser.parse_args()
+from . import Fitbit, FitbitAuth
 
-    if args.ask_password:
-        password = getpass.getpass('Password for {}: '.format(args.username))
 
-    f = Fitbit()
-    r = f.login(username=args.username, password=password)
+def main():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
 
-    user_info = f.user_info
-    print(json.dumps(user_info, indent=2))
+    fa = FitbitAuth(
+        client_id=config['fitbit_auth']['client_id'],
+        client_secret=config['fitbit_auth']['client_secret'],
+    )
+    fa.ensure_access_token()
 
+    f = Fitbit(access_token=fa.access_token['access_token'])
+    print(json.dumps(f.profile, indent=2))
 
     sleep = f.get_sleep(date(2017, 4, 8))
     print(json.dumps(sleep, indent=2))
 
+
+
+if __name__ == '__main__':
+    main()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('--username', required=True)
+    # parser.add_argument('--ask-password', required=True, action='store_true')
+    # args = parser.parse_args()
